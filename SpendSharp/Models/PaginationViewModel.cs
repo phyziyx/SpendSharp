d8bd4511c328bd8d04638997b792b91b0b089f07
phyziyx <96657278+phyziyx@users.noreply.github.com>
2025-09-28 11:40:17 +0500

    ft: Pagination "component"

---
+++ SpendSharp/Models/PaginationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SpendSharp.Models
+{
+    public class PaginationViewModel
+    {
+        public int Page { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public string Controller { get; set; } = "";
+        public string Action { get; set; } = "";
+        public int[] PageSizes { get; set; } = [5, 10, 25, 50];
+    }
+}
